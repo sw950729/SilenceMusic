7a1c367e50b1259ca91edb7e227fe85e70a0b01c
Administrator <18210155854@163.com>
2017-10-16 22:20:48 +0800

    添加了annotation集成

--- app/src/main/java/com/angel/music/MainActivity.java
+++ app/src/main/java/com/angel/music/MainActivity.java
@@ -3,6 +3,9 @@ package com.angel.music;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.androidannotations.annotations.EActivity;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
